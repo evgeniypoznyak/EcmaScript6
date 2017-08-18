630f8075009816b24aac77525f60e14f2547e1e7
Evgeniy Poznyak <ek35mm@gmail.com>
2017-08-18 17:21:35 -0500

    arrow-functions - end Destructuring - beginning

--- src/js/lessons/code_dojo/arrow-functions.js
+++ src/js/lessons/code_dojo/arrow-functions.js
@@ -1,28 +1,66 @@
-let add = (x, y) => x + y;
-
-let square = x => x * x;
-
-let giveMeAnswer = () => 42;
-
-let log = ()=> console.log('poop');
-
-let multiply = (x, y) => x * y;
-
-let getPerson = () => ({name: 'John'});
-
 //IIFE
-(function() {
+/*(function() {
     console.log('IIFE');
-})();
+})();*/
 
 //IIFE Arrow
-((x) => console.log('IIFE ARROW' + x))('!');
+/*((x) => console.log('IIFE ARROW' + x))('!');*/
 
 
-console.log(add(2, 3));
-console.log(square(3));
-console.log(giveMeAnswer());
-log();
-console.log(multiply(2, 2));
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let sum = 0;
+// numbers.forEach(num => sum += num)
+// let squared = numbers.map(n => n * n)
+// console.log(sum);
+// console.log(squared);
 
-console.log(getPerson());
+/*
+let person = {
+    name: 'Bob',
+    greet: function () {
+        console.log(`Hello, my name is ${this.name}`);
+        console.log(this);
+    }
+}
+
+
+person.greet();
+
+let person2 = {
+    name: 'Bob',
+    greet:  function() {
+        var self = this;
+        setTimeout(() => {
+            console.log(`Hello, my name is ${this.name}`);
+            console.log(this);
+        }, 2000);
+    }
+}
+
+
+person2.greet();
+*/
+
+
+let Task = () => console.log('Creating Task...');
+
+let task = new Task();
+
+
+
+// let add = (x, y) => x + y;
+// console.log(add(2, 3));
+// let square = x => x * x;
+// console.log(square(3));
+// let giveMeAnswer = () => 42;
+// console.log(giveMeAnswer());
+// let log = ()=> console.log('poop');
+// log();
+// let multiply = (x, y) => x * y;
+// console.log(multiply(2, 2));
+// let getPerson = () => ({name: 'John'});
+// console.log(getPerson());
+
+
+
+
