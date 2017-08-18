f3f1fed3c7d7e3aa9d1cfce31590fc14efadaf8a
Evgeniy Poznyak <ek35mm@gmail.com>
2017-08-18 13:13:30 -0500

    classes.js

--- src/js/lessons/code_dojo/classes.js
+++ src/js/lessons/code_dojo/classes.js
@@ -3,16 +3,22 @@ class Task {
         this.title = title
         console.log('Создание задачи...');
         Task.count += 1;
-        this.done = false
+        this._done = false
     }
     complete(){
         this.done = true;
         console.log(this.title + ' - выполнено');
     }
 
-    // get done() {
-    //     return this.done === true ? 'Выполнена' : 'Не выполнена';
-    // }
+    get done() {
+        return this._done === true ? 'Выполнена' : 'Не выполнена';
+    }
+
+    set done(value){
+        if (value !== undefined && typeof value === 'boolean') {
+            this._done = value;
+        }else console.error('Ошибка! Укажите значение true или false')
+    }
 
     static getDefaultTitle(){
         return 'Задача';
@@ -23,12 +29,25 @@ class Task {
 Task.count = 0;
 
 let task = new Task('Поехать домой');
-let task2 = new Task();
+console.log(task.done);
+console.log(task._done);
+task.complete()
+console.log(task.done);
+console.log(task._done);
 
-console.log(typeof task);
-console.log(task instanceof Task)
-console.log(task.title);
-console.log(task2.title);
-task.complete();
 
-console.log(Task.count);
+// console.log(task.done, task._done);
+// task.complete()
+// console.log(task.done, task._done);
+
+
+
+// let task2 = new Task();
+//
+// console.log(typeof task);
+// console.log(task instanceof Task)
+// console.log(task.title);
+// console.log(task2.title);
+// task.complete();
+//
+// console.log(Task.count);
