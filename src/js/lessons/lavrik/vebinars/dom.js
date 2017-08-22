8cb675f9bea22d48e44ec6ab7ae1951d9d284471
Evgeniy Poznyak <ek35mm@gmail.com>
2017-08-22 17:53:24 -0500

    dom_v2

--- src/js/lessons/lavrik/vebinars/dom.js
+++ src/js/lessons/lavrik/vebinars/dom.js
@@ -1,1 +1,31 @@
-console.log('dom');
+const div = document.querySelector('#res');
+div.innerHTML = 'CLICK ME!';
+
+div.onclick = function () {
+    div.innerHTML = 'WOW!';
+    div.className = 'test';
+    div.style.color = 'red';
+    div.style.fontSize = '30px';
+}
+
+
+
+
+
+
+
+
+// Вывод всех свойств элемента DOM
+/*
+print_object(div.style);
+
+function print_object(obj) {
+    let res = `<ul>`;
+    for (let i in obj) {
+        res += `<li><b>${i}</b>: ${obj[i]}</li>`
+    }
+    res += `</ul>`;
+
+    document.write(res);
+}
+*/
