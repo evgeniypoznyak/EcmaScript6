f62d6735e830c3a95567155fd9feb65be10c6115
Evgeniy Poznyak <ek35mm@gmail.com>
2017-08-22 16:59:43 -0500

    closures callbacks + call()

--- src/js/lessons/lavrik/vebinars/callbacks_clossures_contects.js
+++ src/js/lessons/lavrik/vebinars/callbacks_clossures_contects.js
@@ -8,29 +8,48 @@ window.onload = function () {
 
     function activeItem() {
         this.classList.toggle('red');
+        fade(this, 1000, function () {
+            this.style.display = 'none';
+            console.log('poop');
+            console.log(this);
+        })
     }
 
-/*    setInterval(function () {
-     let rand = mtRand(0, items.length - 1);
-     activeItem.call(items[rand]);
-     //   items[rand].classList.toggle('red');
-      //  console.log(rand);
-    }, 500);*/
 
+    function fade(elem, t, f) {
+        var fps = 50;
+        var time = t || 500;
+        var steps = time / fps;
+        var op = 1;
+        var d0 = op / steps;
+
+        var callback = f || function () {
+        }
+        
+        var timer = setInterval(function () {
+            op -= d0;
+            elem.style.opacity = op;
+            steps--;
+            if (steps === 0) {
+                clearInterval(timer);
+                callback.call(elem);
+            }
+        }, 1000 / fps)
+    }
+
+
+/*
     function mtRand(min, max) {
         return Math.floor(Math.random() * (max - min + 1));
     }
+    */
 
-
-
-
-
-
-
-
-
-
-
+    /*    setInterval(function () {
+         let rand = mtRand(0, items.length - 1);
+         activeItem.call(items[rand]);
+         //   items[rand].classList.toggle('red');
+          //  console.log(rand);
+        }, 500);*/
     /*    document.querySelector('.items').addEventListener('click', function () {
     this.classList.toggle('red');
 })*/
