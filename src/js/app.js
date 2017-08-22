d9535d2f60db06537de14cf022e99ac64d418044
Evgeniy Poznyak <ek35mm@gmail.com>
2017-08-22 17:07:49 -0500

    dom

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,6 @@
 //import 'jquery';
 import '../css/main.scss';
 
-import '../js/lessons/lavrik/vebinars/index.html'
 
-
-//import './js/max/max'
-//import './js/promises/promises';
-
-//import './lessons/lavrik/lavrik';
-
-
-import './lessons/lavrik/vebinars/callbacks_clossures_contects'
+import './lessons/lavrik/vebinars/dom'
 
